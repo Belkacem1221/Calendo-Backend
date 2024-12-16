6b2cc12e26d65d1827627f2efcd12f28a9c28dfd
YanisBY <yaciniyanisb@gmail.com>
2024-12-16 15:25:41 -0500

    server.js bug fix

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ app.use(express.json()); // Parse incoming JSON requests
 //app.use('/api/users', userRoutes);
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI, {})
 .then(() => console.log('MongoDB connected!'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
