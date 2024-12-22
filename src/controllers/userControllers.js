a1f079858f5944646f7b35d84df07976dce962a4
YanisBY <yaciniyanisb@gmail.com>
2024-12-22 15:18:33 -0500

    getUserById added

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -26,3 +26,18 @@ exports.getAllUsers = async (req, res) => {
     res.status(500).json({ message: 'Error fetching users', error: err });
   }
 };
+
+// Get a user by ID
+exports.getUserById = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching user', error: err });
+  }
+};
