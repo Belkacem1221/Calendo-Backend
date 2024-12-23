dcbc081fb8144cf38d6fffb09197608aa88d1d0e
YanisBY <yaciniyanisb@gmail.com>
2024-12-22 22:32:21 -0500

    admin routes added

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,12 +4,15 @@ const UserController = require('../controllers/userControllers');
 const authenticateToken = require('../middlewares/authMiddleware');
 const authenticateAdmin = require('../middlewares/authenticateAdmin');
 
-// Protected routes
+//Admin routes
 router.get('/', authenticateToken, authenticateAdmin, UserController.getAllUsers); // Admin can access all users
+router.delete('/:id', authenticateToken, authenticateAdmin, UserController.deleteUser); // Admin can delete user
+router.patch('/:id/role', authenticateToken, authenticateAdmin, UserController.changeUserRole); // Admin can change user role
+
+
+// Protected routes
 router.get('/:id', authenticateToken, UserController.getUserById); // Get user by ID
 router.put('/:id', authenticateToken, UserController.updateUser); // Update user
-router.delete('/:id', authenticateToken, authenticateAdmin, UserController.deleteUser); // Admin can delete user
 router.patch('/:id/password', authenticateToken, UserController.updatePassword); // Update password
-router.patch('/:id/role', authenticateToken, authenticateAdmin, UserController.changeUserRole); // Admin can change user role
 
 module.exports = router;
