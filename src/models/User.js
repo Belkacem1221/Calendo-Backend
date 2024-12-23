01cb88462b954c55b82ef07b3eb1f3624c7ea342
YanisBY <yaciniyanisb@gmail.com>
2024-12-22 22:16:37 -0500

    Role added in user schema

--- src/models/User.js
+++ src/models/User.js
@@ -21,6 +21,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       minlength: 6,
     },
+    role: {
+      type: String,
+      enum: ['user', 'admin'], // Définir les rôles possibles
+      default: 'user', // Rôle par défaut
+    },
   },
   { timestamps: true }
 );
