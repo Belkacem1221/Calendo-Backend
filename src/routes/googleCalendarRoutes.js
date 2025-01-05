3022a56fee09d52b502ce572c9b9d1783e89e998
YanisBY <yaciniyanisb@gmail.com>
2025-01-05 13:09:01 -0500

    oauthMiddleware added in googleCalendarRoutes

--- src/routes/googleCalendarRoutes.js
+++ src/routes/googleCalendarRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { getCalendarEvents, createCalendarEvent, updateCalendarEvent } = require('../controllers/googleCalendarController');
+const oauthMiddleware = require('../middlewares/oauthMiddleware');
 const router = express.Router();
 
-router.get('/events', getCalendarEvents);
+// Route to get calendar events
+router.get('/events', oauthMiddleware, getCalendarEvents);
 
-router.post('/create-event', createCalendarEvent);
+// Route to create a calendar event
+router.post('/create-event', oauthMiddleware, createCalendarEvent);
 
-router.put('/update-event/:eventId', updateCalendarEvent);
+// Route to update a calendar event
+router.put('/update-event/:eventId', oauthMiddleware, updateCalendarEvent);
 
 module.exports = router;
